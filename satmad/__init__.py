ad8ac2dc9e9cc554b438e83afc588f983872fde5
egemenimre <s.egemen.imre@gmail.com>
2020-05-02 23:52:46 +0200

    cleaned up astropy boilerplate

--- satmad/__init__.py
+++
@@ -1,8 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
-# ----------------------------------------------------------------------------
-
