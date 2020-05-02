d63942fb48673e4d05e809533fed193e8f4d028e
Egemen Imre <62849579+egemenimre@users.noreply.github.com>
2020-05-02 23:44:25 +0200

    added `master_doc` and `source_suffix`

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
