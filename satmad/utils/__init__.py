1038f0ebc0417cbe8321acc7201b53fa38e39fc5
egemenimre <s.egemen.imre@gmail.com>
2020-05-04 10:34:35 +0200

    Changed doc stub

--- satmad/utils/__init__.py
+++ satmad/utils/__init__.py
@@ -1,4 +1,4 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Helpers and utility functions.
 
-# This sub-module is destined for common non-package specific utility
-# functions.
+"""
