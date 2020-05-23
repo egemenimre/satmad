fa0a249c4488d8b1399204346998f5ed3ed6660f
egemenimre <s.egemen.imre@gmail.com>
2020-05-24 00:13:57 +0200

    added file comment with copyright and license

--- satmad/__init__.py
+++ satmad/__init__.py
@@ -1,3 +1,8 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
 """
 satmad
 =========
--- satmad/coordinates/__init__.py
+++ satmad/coordinates/__init__.py
@@ -1,3 +1,8 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
 """
 This subpackage contains classes and functions for coordinates of satellites
 and other objects not defined in Astropy.
