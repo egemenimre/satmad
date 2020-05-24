8d04adf7cbd21f91bce9144d16522ad37ed08bcd
egemenimre <s.egemen.imre@gmail.com>
2020-05-24 16:27:48 +0200

    Intersect, Union etc Functionality added.
    - Added _is_intesecting(), _is_within_interval() and _contains functionality and associated unit testing.
    - Added _union() and intersect() functionality and associated unit testing.
    - Added _expand(), _duration() and _interval_equals() functionality and associated unit testing.

--- satmad/utils/tests/test_timeinterval.py
+++ satmad/utils/tests/test_timeinterval.py
@@ -11,7 +11,37 @@ import numpy as np
 import pytest
 from astropy.time import Time, TimeDelta
 
-from satmad.utils.timeinterval import TimeInterval
+from satmad.utils.timeinterval import (
+    TimeInterval,
+    _contains,
+    _intersect,
+    _is_intersecting,
+    _is_within_interval,
+    _union,
+    _expand,
+    _interval_equals,
+)
+
+before = (
+    Time("2020-04-09T00:00:00", scale="utc"),
+    Time("2020-04-11T00:00:00", scale="utc"),
+)
+within = (
+    Time("2020-04-11T00:05:00", scale="utc"),
+    Time("2020-04-11T00:08:00", scale="utc"),
+)
+intersect = (
+    Time("2020-04-10T00:00:00", scale="utc"),
+    Time("2020-04-11T00:08:00", scale="utc"),
+)
+exact = (
+    Time("2020-04-11T00:00:00", scale="utc"),
+    Time("2020-04-11T00:10:00", scale="utc"),
+)
+after = (
+    Time("2020-04-11T00:10:00", scale="utc"),
+    Time("2020-04-12T00:00:00", scale="utc"),
+)
 
 
 @pytest.fixture
@@ -45,12 +75,6 @@ def test_init_with_end_times(init_times, durations):
 
     end_times = init_times + durations
 
-    # combo = Time(
-    #     np.concatenate((init_times.isot, end_times.isot), axis=0)
-    #     .reshape(2, 3)
-    #     .transpose()
-    # )
-
     intervals = TimeInterval(init_times, end_times)
 
     truth_txt = (
@@ -60,3 +84,96 @@ def test_init_with_end_times(init_times, durations):
     )
 
     assert truth_txt == str(intervals)
+
+
+def test_is_intersecting(init_times, durations):
+    """Test `is_intersecting` method."""
+    interval = TimeInterval(init_times, durations).get_interval(0)
+
+    assert _is_intersecting(before, interval) is False
+    assert _is_intersecting(within, interval) is True
+    assert _is_intersecting(intersect, interval) is True
+    assert _is_intersecting(exact, interval) is True
+    assert _is_intersecting(after, interval) is False
+
+
+def test_contains(init_times, durations):
+    """Test `_contains` method."""
+    interval = TimeInterval(init_times, durations).get_interval(0)
+
+    assert _contains(before, interval) is False
+    assert _contains(within, interval) is True
+    assert _contains(exact, interval) is True
+    assert _contains(intersect, interval) is False
+    assert _contains(after, interval) is False
+
+
+def test_is_within_interval(init_times, durations):
+    """Test `is_within_interval` method."""
+    interval = TimeInterval(init_times, durations).get_interval(0)
+
+    t_before = Time("2020-04-09T00:00:00", scale="utc")
+    t_eqinit = Time("2020-04-11T00:00:00", scale="utc")
+    t_within = Time("2020-04-11T00:05:00", scale="utc")
+    t_eqend = Time("2020-04-11T00:10:00", scale="utc")
+    t_after = Time("2020-04-11T12:00:00", scale="utc")
+
+    assert _is_within_interval(t_before, interval) is False
+    assert _is_within_interval(t_eqinit, interval) is True
+    assert _is_within_interval(t_within, interval) is True
+    assert _is_within_interval(t_eqend, interval) is True
+    assert _is_within_interval(t_after, interval) is False
+
+
+def test_intersect(init_times, durations):
+    """Test `_intersect` method."""
+    interval = TimeInterval(init_times, durations).get_interval(0)
+
+    assert _intersect(before, interval) is None
+    assert _interval_equals(_intersect(within, interval), within)
+    assert _interval_equals(
+        _intersect(intersect, interval), (interval[0], intersect[1])
+    )
+    assert _interval_equals(_intersect(exact, interval), exact)
+    assert _intersect(after, interval) is None
+
+
+def test_union(init_times, durations):
+    """Test `_union` method."""
+    interval = TimeInterval(init_times, durations).get_interval(0)
+
+    assert _union(before, interval) is None
+    assert _interval_equals(_union(within, interval), interval)
+    assert _interval_equals(_union(intersect, interval), (intersect[0], interval[1]))
+    assert _interval_equals(_union(exact, interval), interval)
+    assert _union(after, interval) is None
+
+
+def test_expand():
+    """Test `_expand` method."""
+    with pytest.raises(ValueError):
+        # Test expansion
+        expanded = _expand(
+            within,
+            delta_begin=TimeDelta(5 * 60, format="sec"),
+            delta_end=TimeDelta(2 * 60, format="sec"),
+        )
+        assert _interval_equals(expanded, exact)
+
+        # Test shrinkage
+        shrunk = _expand(
+            exact,
+            delta_begin=TimeDelta(-5 * 60, format="sec"),
+            delta_end=TimeDelta(-2 * 60, format="sec"),
+        )
+        assert _interval_equals(shrunk, within)
+
+        # Test shrink to zero - this raises a ValueError
+        assert _expand(within, delta_begin=TimeDelta(-3 * 60, format="sec"))
+
+        # Test a negative duration shrinkage - this raises a ValueError
+        assert _expand(
+            within,
+            delta_begin=TimeDelta(-5 * 60, format="sec"),
+            delta_end=TimeDelta(-5 * 60, format="sec"),
+        )
--- satmad/utils/timeinterval.py
+++ satmad/utils/timeinterval.py
@@ -14,6 +14,11 @@ such as operations between `TimeInterval` objects (union, intersection etc.).
 from astropy import units as u
 from astropy.time import Time, TimeDelta
 
+_EPS_TIME = 10 * u.us
+"""Allowable time threshold, this much 'out of bounds' is allowed when assuming two
+instances of time are *practically* equal. This helps with floating point artifacts such as
+round-off errors."""
+
 
 class TimeInterval:
     """
@@ -31,14 +36,11 @@ class TimeInterval:
 
     """
 
-    # TODO
-    #     flag end inclusive
-    #     flag begin inclusive
-
-    _EPS_TIME = 10 * u.us
-    """Allowable time threshold, this much 'out of bounds' is allowed when handling
-    requested interpolation times. This helps with floating point artifacts such as
-    round-off errors."""
+    # TODO check for out of validity errors
+    #  when doing Union with an external interval
+    #
+    # TODO  take union etc methods to within class
+    #  with thin wrappers and checks and multiple items
 
     def __init__(self, init_times, end_times, replicate=False):
 
@@ -116,3 +118,264 @@ class TimeInterval:
             txt += f"  {str(getattr(self._end_times[i], self._end_times.format))} ]\n"
 
         return txt
+
+
+def _is_within_interval(time, ref_interval, inclusive=True):
+    """
+    Checks whether the requested time is within the reference interval.
+
+    - If the `inclusive` flag is True: ref_interval_begin <= time <= ref_interval_end
+    - If the `inclusive` flag is False: ref_interval_begin < time < ref_interval_end
+
+    Parameters
+    ----------
+    time : Time
+        time to be checked
+    ref_interval : (Time, Time)
+        Reference interval (Tuple of initial time and end time)
+    inclusive : bool
+        True if max and min boundaries are included in the check, False otherwise
+
+    Returns
+    -------
+    bool
+        True if time is within the reference interval, False otherwise
+    """
+    if inclusive:
+        if ref_interval[0].value <= time.value <= ref_interval[1].value:
+            return True
+        else:
+            return False
+    else:
+        if ref_interval[0].value < time.value < ref_interval[1].value:
+            return True
+        else:
+            return False
+
+
+def _is_intersecting(interval, ref_interval):
+    """
+    Checks whether the requested interval intersects (or is contained within)
+    the reference interval.
+
+    Parameters
+    ----------
+    interval : (Time, Time)
+        Check interval (Tuple of initial time and end time)
+    ref_interval : (Time, Time)
+        Reference interval (Tuple of initial time and end time)
+
+    Returns
+    -------
+    bool
+        True if check interval intersects with the reference interval, False otherwise
+    """
+    if _contains(interval, ref_interval):
+        # interval is contained or the same as ref interval
+        return True
+    elif _is_within_interval(interval[0], ref_interval, False) or _is_within_interval(
+        interval[1], ref_interval, False
+    ):
+        # at least interval_begin or interval_end is within the interval
+        return True
+    else:
+        return False
+
+
+def _contains(interval, ref_interval):
+    """
+    Checks whether the requested interval is contained within the reference interval.
+
+    Parameters
+    ----------
+    interval : (Time, Time)
+        Check interval (Tuple of initial time and end time)
+    ref_interval : (Time, Time)
+        Reference interval (Tuple of initial time and end time)
+
+    Returns
+    -------
+    bool
+        True if check interval is contained within the reference interval,
+        False otherwise
+    """
+    if _is_within_interval(interval[0], ref_interval, True) and _is_within_interval(
+        interval[1], ref_interval, True
+    ):
+        return True
+    else:
+        return False
+
+
+def _intersect(interval, ref_interval):
+    """
+    Intersection operator for two time intervals.
+
+    Returns a new interval that is
+    the Intersection of two intervals, or None if there is no intersection.
+
+    Parameters
+    ----------
+    interval : (Time, Time)
+        Check interval (Tuple of initial time and end time)
+    ref_interval : (Time, Time)
+        Reference interval (Tuple of initial time and end time)
+
+    Returns
+    -------
+    (Time, Time)
+        A new interval (Tuple of initial time and end time) that is
+        the Intersection of two intervals, or `None` if there is no intersection
+
+    """
+    if _is_intersecting(interval, ref_interval):
+
+        if interval[0] < ref_interval[0]:
+            # interval begins before ref_interval - set ref_interval begin
+            new_init = ref_interval[0]
+        else:
+            # interval begins after ref_interval - set interval begin
+            new_init = interval[0]
+
+        if ref_interval[1] < interval[1]:
+            # interval ends after ref_interval - set ref_interval end
+            new_end = ref_interval[1]
+        else:
+            # interval ends before ref_interval - set interval end
+            new_end = interval[1]
+
+        return new_init, new_end
+    else:
+        # No intersection found
+        return None
+
+
+def _union(interval, ref_interval):
+    """
+    Union operator for two time intervals.
+
+    Returns a new interval that is
+    the Union of two intervals, or None if there is no intersection.
+
+    Parameters
+    ----------
+    interval : (Time, Time)
+        Check interval (Tuple of initial time and end time)
+    ref_interval : (Time, Time)
+        Reference interval (Tuple of initial time and end time)
+
+    Returns
+    -------
+    (Time, Time)
+        A new interval (Tuple of initial time and end time) that is
+        the Union of two intervals, or `None` if there is no intersection
+
+    """
+    if _is_intersecting(interval, ref_interval):
+
+        if interval[0] < ref_interval[0]:
+            # interval begins before ref_interval - set interval begin
+            new_init = interval[0]
+        else:
+            # interval begins after ref_interval - set ref_interval begin
+            new_init = ref_interval[0]
+
+        if ref_interval[1] < interval[1]:
+            # interval ends after ref_interval - set interval end
+            new_end = interval[1]
+        else:
+            # interval ends before ref_interval - set ref_interval end
+            new_end = ref_interval[1]
+
+        return new_init, new_end
+    else:
+        # No intersection found
+        return None
+
+
+def _duration(interval):
+    """
+    Computes the duration of an interval.
+
+    Parameters
+    ----------
+    interval : (Time, Time)
+        Interval (Tuple of initial time and end time)
+
+    Returns
+    -------
+    duration : TimeDelta
+        Duration of the interval (can be negative)
+    """
+    return interval[1] - interval[0]
+
+
+def _expand(interval, delta_begin=0, delta_end=0):
+    """
+    Expands (or shrinks) the given interval.
+
+    Generates a new tuple, where:
+    - new interval begin: interval[0] - delta_begin
+    - new interval end: interval[1] + delta_end
+
+    Negative begin and/or end times are possible (to shrink the interval),
+    though values ending in a negative interval will throw a `ValueError`.
+
+    Parameters
+    ----------
+    interval : (Time, Time)
+        Interval (Tuple of initial time and end time)
+    delta_begin : TimeDelta
+        The delta time to expand the beginning of the interval
+        (or to shrink, with negative values)
+    delta_end : TimeDelta
+        The delta time to expand the end of the interval
+        (or to shrink, with negative values)
+    Returns
+    -------
+    (Time, Time)
+        A new interval (Tuple of initial time and end time) that is
+        the result of the requested change
+
+    Raises
+    ------
+    ValueError
+        Raised if the requested expansion results in a negative duration interval
+    """
+    new_interval = (interval[0] - delta_begin, interval[1] + delta_end)
+
+    if _duration(new_interval) > _EPS_TIME:
+        return new_interval
+    else:
+        raise ValueError(
+            "Duration of the expanded/shrunk interval is negative or zero."
+        )
+
+
+def _interval_equals(interval, ref_interval):
+    """
+    Checks whether two intervals are (almost) equal in value.
+
+    If the two begin or end values are as close as `_EPS_TIME`, then they are assumed
+    equal in value.
+
+    Parameters
+    ----------
+     interval : (Time, Time)
+        Check interval (Tuple of initial time and end time)
+    ref_interval : (Time, Time)
+        Reference interval (Tuple of initial time and end time)
+
+    Returns
+    -------
+    bool
+        True if interval begin and end are (almost) equal, False otherwise
+
+    """
+    begin_equal = abs(interval[0] - ref_interval[0]) < _EPS_TIME
+    end_equal = abs(interval[1] - ref_interval[1]) < _EPS_TIME
+
+    if begin_equal and end_equal:
+        return True
+    else:
+        return False
