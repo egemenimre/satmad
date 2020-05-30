ed148310b31fb2ecb0008fce64d5100e4013b098
egemenimre <s.egemen.imre@gmail.com>
2020-05-30 16:03:23 +0200

    all single quotes converted to double quotes by autoformatter (probably black)

--- docs/conf.py
+++ docs/conf.py
@@ -13,53 +13,54 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = 'satmad'
-copyright = '2020, Egemen Imre'
-author = 'Egemen Imre'
+project = "satmad"
+copyright = "2020, Egemen Imre"
+author = "Egemen Imre"
 
-version = '0.0.1'
+version = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # By default, highlight as Python 3.
-highlight_language = 'python3'
+highlight_language = "python3"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',  # auto api generation
-    'sphinx.ext.napoleon',  # numpy support
-    'sphinx.ext.mathjax',  # LaTex style math
-    'sphinx.ext.graphviz',  # Dependency diagrams
-    'sphinx.ext.intersphinx',  # Link mapping to external projects
+    "sphinx.ext.autodoc",  # auto api generation
+    "sphinx.ext.napoleon",  # numpy support
+    "sphinx.ext.mathjax",  # LaTex style math
+    "sphinx.ext.graphviz",  # Dependency diagrams
+    "sphinx.ext.intersphinx",  # Link mapping to external projects
+    # "sphinx.ext.doctest",  # Doctest
 ]
 numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
-#Intersphinx configuration
+# Intersphinx configuration
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'astropy': ('http://docs.astropy.org/en/stable/', None),
-    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
-    'matplotlib': ('https://matplotlib.org', None)
+    "python": ("https://docs.python.org/3", None),
+    "astropy": ("http://docs.astropy.org/en/stable/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "matplotlib": ("https://matplotlib.org", None),
 }
 
 # -- Options for HTML output -------------------------------------------------
@@ -67,9 +68,9 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
