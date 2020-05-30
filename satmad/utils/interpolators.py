33b6e377e76498cb2674bbd30d1a015c38c1da98
egemenimre <s.egemen.imre@gmail.com>
2020-05-30 16:34:10 +0200

    Modified copyright year with a {YEAR} field

--- satmad/utils/interpolators.py
+++ satmad/utils/interpolators.py
@@ -1,6 +1,6 @@
 # SatMAD: Satellite Mission Analysis and Design for Python
 #
-# Copyright (C) 2020 Egemen Imre
+# Copyright (C) ${YEAR} Egemen Imre
 #
 # Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
 """
