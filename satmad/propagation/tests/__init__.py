f82d384b9d088d98e2c8158676bdc27c57f561bf
egemenimre <s.egemen.imre@gmail.com>
2020-06-07 22:47:03 +0200

    init empty files with the propagation package

---
+++ satmad/propagation/tests/__init__.py
@@ -0,0 +1,9 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Satellite propagation tests.
+
+"""
