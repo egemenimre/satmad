2e6ac588884582b6518de926c08a30f4d90c043c
egemenimre <s.egemen.imre@gmail.com>
2020-07-08 22:32:29 +0200

    Initialised Discrete Time Intervals and relevant tests.

---
+++ satmad/utils/discrete_time_intervals.py
@@ -0,0 +1,258 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Discrete time intervals management and event finding methods.
+
+"""
+from astropy import units as u
+from astropy.timeseries import TimeSeries
+from scipy import interpolate
+
+from satmad.utils.timeinterval import TimeInterval, TimeIntervalList
+
+
+class DiscreteTimeIntervals:
+    """
+    Time events finding and storage class.
+
+    An *time event* is described as a function value crossing a certain threshold in
+    increasing (negative to positive) and decreasing (positive to negative) directions,
+    defining a time interval.
+
+    The class initialises interpolators to find where the `value_list` crosses the
+    `crossing_value`, which is essentially a root finding problem.
+
+    Parameters
+    ----------
+    time_list : Time
+        List of time instances
+    value_list : ndarray or list
+        List of values corresponding to time instances (should be of the same size as
+        `time_list`)
+    crossing_value : float
+        Crossing value for the `value_list`
+    neg_to_pos_is_start : bool
+        If `True` value turning from negative to positive marks a *start* event,
+        otherwise marks an *end* event
+
+    Raises
+    ------
+    ValueError
+        `time_list` and `value_list` are of different sizes
+    """
+
+    def __init__(
+        self, time_list, value_list, crossing_value=0, neg_to_pos_is_start=True
+    ):
+
+        # Check whether time_list and value_list size are equal
+        if len(time_list) != len(value_list):
+            raise ValueError(
+                f"Time list and value list are of different sizes. "
+                f"({len(time_list)} vs. ({len(value_list)}))"
+            )
+
+        self.search_interval = TimeInterval(time_list[0], time_list[-1])
+
+        # Convert time to "days since epoch"
+        t_float_list = (time_list - time_list[0]).jd
+
+        # Init interpolators (for splines, root finding possible for 3rd degree
+        # (cubics) only)
+        self._interpolator = interpolate.CubicSpline(
+            t_float_list, value_list - crossing_value,
+        )
+
+        self._deriv_interpolator = self._interpolator.derivative()
+
+        # Find start / end intervals
+        self.start_end_intervals = self._find_intervals(time_list, neg_to_pos_is_start)
+
+        # Find max / min events
+        self.max_min_table = self._find_extrema_events(time_list[0])
+
+        # Return the table to absolute values
+        self.max_min_table["value"] = self.max_min_table["value"] + crossing_value
+
+    def _find_intervals(self, time_list, neg_to_pos_is_start):
+        """
+        Finds the start / end intervals within the time list.
+
+        Parameters
+        ----------
+        time_list : Time
+            List of time instances
+        neg_to_pos_is_start : bool
+            If `True` value turning from negative to positive marks a *start* event,
+            otherwise marks an *end* event
+
+        Returns
+        -------
+        intervals : TimeIntervalList
+            Time intervals marked with start and end events
+        """
+
+        init_time = time_list[0]
+        end_time = time_list[-1]
+
+        # *** Find start / end events (do not extrapolate to find roots) ***
+        # No roots mean no events during this time
+        start_end_events = self._interpolator.roots(extrapolate=False)
+
+        intervals = []
+        if start_end_events.size != 0:
+            # there should be some roots within the search range to check for intervals
+
+            # classify start / end events and fill the timetable
+            events = self._classify_start_end_events(
+                init_time, start_end_events, neg_to_pos_is_start
+            )
+
+            # Put events into intervals
+            event_start = init_time
+
+            for event in events:
+                # ignore events outside the search interval
+                if self.search_interval.is_in_interval(event["time"]):
+                    if event["type"] == "start":
+                        event_start = event["time"]
+                    else:
+                        intervals.append(TimeInterval(event_start, event["time"]))
+
+            # check for the last event - force end time if it is out of search bounds
+            if events[-1]["type"] == "end":
+                if self.search_interval.is_in_interval(events[-1]["time"]):
+                    intervals.append(TimeInterval(event_start, events[-1]["time"]))
+                else:
+                    intervals.append(
+                        TimeInterval(event_start, self.search_interval.end)
+                    )
+
+        # If interval list is empty by this stage, either the complete duration is
+        # is a valid interval with no event (e.g. a continuously visible satellite)
+        # or the complete duration is an invalid interval with no event (e.g. a
+        # satellite continuously outside visibility)
+        if not intervals:
+            mid_time = 0.5 * (self.search_interval.end - self.search_interval.start)
+            value = self._interpolator(mid_time.to_value(u.day))
+
+            if neg_to_pos_is_start:
+                if value > 0:
+                    # event already started
+                    intervals.append(
+                        TimeInterval(
+                            self.search_interval.start, self.search_interval.end
+                        )
+                    )
+            else:
+                if value < 0:
+                    # event already started
+                    intervals.append(
+                        TimeInterval(
+                            self.search_interval.start, self.search_interval.end
+                        )
+                    )
+
+        # generate the list of time intervals
+        return TimeIntervalList(intervals, start_valid=init_time, end_valid=end_time)
+
+    def _classify_start_end_events(
+        self, init_time, start_end_events, neg_to_pos_is_start
+    ):
+        """
+        Classify the list of events into a timetable of start and end events.
+
+        Parameters
+        ----------
+        init_time : Time
+            Initial time
+        start_end_events : ndarray
+            Array of start and end event times in days, starting from `init_time`
+        neg_to_pos_is_start : bool
+            If `True` value turning from negative to positive marks a *start* event,
+            otherwise marks an *end* event
+
+        Returns
+        -------
+        events_table : TimeSeries
+            Timetable of start and end events
+        """
+
+        # loop through each root and classify them as start / end events
+        events_list = []
+        for event_time in start_end_events:
+
+            deriv = self._deriv_interpolator(event_time)
+
+            if neg_to_pos_is_start:
+                if deriv > 0:
+                    events_list.append("start")
+                else:
+                    events_list.append("end")
+            else:
+                if deriv < 0:
+                    events_list.append("start")
+                else:
+                    events_list.append("end")
+
+        # init events table
+        events_table = TimeSeries(
+            time=init_time + start_end_events * u.day, data={"type": events_list}
+        )
+
+        return events_table
+
+    def _find_extrema_events(self, init_time):
+        """
+        Finds the extrema (max/min) events, contained within the time intervals.
+
+        Parameters
+        ----------
+        init_time : Time
+            Initial time
+
+        Returns
+        -------
+        max_min_table : TimeSeries
+            Timetable of max and min events
+
+        """
+
+        # *** Find max / min events ***
+        min_max_events = self._deriv_interpolator.roots()
+
+        # loop through each root and classify them as max / min events
+        events_list = []
+        value_list = []
+        for event_time in min_max_events:
+            value = self._interpolator(event_time)
+            value_list.append(value)
+            if value > 0:
+                events_list.append("max")
+            else:
+                events_list.append("min")
+
+        # init events table
+        max_min_table = TimeSeries(
+            time=init_time + min_max_events * u.day,
+            data={"type": events_list, "value": value_list},
+        )
+
+        # Filter the events to those within the intervals
+        i = 0
+        remove_indexes = []
+        for event_row in max_min_table:
+            event_within_interval = self.start_end_intervals.is_in_interval(
+                event_row["time"]
+            )
+            if not event_within_interval:
+                remove_indexes.append(i)
+            i = i + 1
+
+        # Remove the ones outside the intervals
+        max_min_table.remove_rows(remove_indexes)
+
+        return max_min_table
---
+++ satmad/utils/tests/test_events.py
@@ -0,0 +1,214 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Tests the `discrete_time_intervals` package functionalities.
+
+"""
+import pytest
+from astropy import units as u
+from astropy.coordinates import AltAz, EarthLocation
+from astropy.time import Time, TimeDelta
+
+from satmad.coordinates.tests.test_trajectory import (
+    generate_timelist,
+    propagation_engine,
+)
+from satmad.propagation.tle import TLE
+from satmad.utils.discrete_time_intervals import DiscreteTimeIntervals
+
+
+def prepare_timelist(init_time, duration, steps):
+    """Prepares time list setup."""
+
+    dt, time_list = generate_timelist(init_time, duration, steps)
+
+    return {"dt": dt, "time_list": time_list}
+
+
+def generate_leo_trajectory(time_list):
+    """Generates the LEO trajectory."""
+
+    # Init TLE
+    line1 = "1 37791U 11044D   15230.18696540  .00000191  00000-0  45093-4 0  9993"
+    line2 = "2 37791 098.1618 317.9053 0023092 067.3069 293.0602 14.64400558213855"
+
+    trajectory = propagation_engine(line1, line2, time_list)["traj"]
+
+    return trajectory
+
+
+def generate_geo_42e_trajectory(time_list):
+    """Generates a GEO (42 deg East) trajectory."""
+
+    # Init TLE
+    tle = TLE.init_geo(Time("2015-10-04T00:00:00.000", scale="utc"), 42 * u.deg)
+    line1, line2 = tle.export_tle()
+
+    trajectory = propagation_engine(line1, line2, time_list)["traj"]
+
+    return trajectory
+
+
+def generate_geo_140w_trajectory(time_list):
+    """Generates a GEO (140 deg West) trajectory."""
+
+    # Init TLE
+    tle = TLE.init_geo(Time("2015-10-04T00:00:00.000", scale="utc"), -140 * u.deg)
+    line1, line2 = tle.export_tle()
+
+    trajectory = propagation_engine(line1, line2, time_list)["traj"]
+
+    return trajectory
+
+
+def event_engine(trajectory_generator, init_time, duration, steps):
+    """Generates events list to be tested."""
+    time_list = prepare_timelist(init_time, duration, steps)["time_list"]
+    trajectory = trajectory_generator(time_list)
+
+    # Init location - default Ellipsoid is WGS84
+    ground_loc = EarthLocation(lat=41.015137, lon=28.979530, height=100 * u.m)
+
+    # generate the alt-az list
+    sat_alt_az_list = trajectory.coord_list.transform_to(
+        AltAz(location=ground_loc, obstime=time_list)
+    )
+
+    # Find intervals in data
+    events = DiscreteTimeIntervals(time_list, sat_alt_az_list.alt.deg, 5)
+
+    return events
+
+
+@pytest.mark.parametrize(
+    "steps, time_error, ang_error_1, ang_error_2",
+    [
+        (4000, 0.13 * u.ms, 9.0e-7 * u.deg, 0.02 * u.deg),
+        (2000, 2.0 * u.ms, 1.6e-4 * u.deg, 0.12 * u.deg),
+        (1000, 160 * u.ms, 2.1e-3 * u.deg, 3.80 * u.deg),
+    ],
+)
+def test_basic_pass_find(steps, time_error, ang_error_1, ang_error_2):
+    """Tests the basic event finding functionality."""
+    # test init time
+    init_time = Time("2015-10-04T00:00:00.000", scale="utc")
+
+    duration = TimeDelta(1.0, format="jd")
+
+    events = event_engine(generate_leo_trajectory, init_time, duration, steps)
+
+    # print(events.start_end_intervals)
+    # print(events.max_min_table)
+
+    # Check max events
+    # ----------------
+    # Case 1: "2015-10-04T11:51:44.967501459", alt: 6.914915588508499 deg
+    diff = (events.max_min_table[2]["value"] - 6.914915588508499) * u.deg
+
+    # print(f"diff : {diff} --- (stepsize: {(duration/steps).to(u.s)})")
+    # print(
+    #     f'{events.max_min_table[2]["time"].datetime64} **** 2015-10-04T11:51:44.967501459'
+    # )
+
+    assert pytest.approx(diff.to_value(u.deg), abs=ang_error_1.to_value(u.deg)) == 0.0
+
+    # Case 2: "2015-10-04T10:15:28.951945200", alt: 63.38630365664624 deg
+    diff = (events.max_min_table[1]["value"] - 63.38630365664624) * u.deg
+
+    # print(f"diff : {diff} --- (stepsize: {(duration/steps).to(u.s)})")
+    # print(
+    #     f'{events.max_min_table[1]["time"].datetime64} **** 2015-10-04T10:15:28.951945200'
+    # )
+    # time error:
+    # - exact match for 21 sec timestep
+    # - 2.5 sec off for 43 sec timestep
+    # - 6.2 sec off for 86 sec timestep
+
+    assert pytest.approx(diff.to_value(u.deg), abs=ang_error_2.to_value(u.deg)) == 0.0
+
+    # Check start / end intervals
+    # ---------------------------
+    diff = (
+        events.start_end_intervals.get_interval(2).start
+        - Time("2015-10-04T11:49:36.742030377")
+    ).to(u.ms)
+
+    # print(f"diff : {diff} --- (stepsize: {(duration/steps).to(u.s)})")
+
+    assert pytest.approx(diff.to_value(u.ms), abs=time_error.to_value(u.ms)) == 0.0
+
+    diff = (
+        events.start_end_intervals.get_interval(4).end
+        - Time("2015-10-04T21:23:19.417927488")
+    ).to(u.ms)
+
+    # print(f"diff : {diff} --- (stepsize: {(duration/steps).to(u.s)})")
+
+    assert pytest.approx(diff.to_value(u.ms), abs=time_error.to_value(u.ms)) == 0.0
+
+
+def test_borderline():
+    """Tests the borderline cases."""
+    # test init time
+    init_time = Time("2015-10-04T08:36:00.000", scale="utc")
+
+    duration = Time("2015-10-04T21:20:00.000", scale="utc") - init_time
+
+    steps = 4000
+
+    events = event_engine(generate_leo_trajectory, init_time, duration, steps)
+
+    print(f"search: {events.search_interval}")
+    print(events.start_end_intervals)
+
+    assert (
+        str(events.start_end_intervals.get_interval(0).start)
+        == "2015-10-04T08:36:00.000"
+    )
+
+    assert (
+        str(events.start_end_intervals.get_interval(4).end) == "2015-10-04T21:19:48.540"
+    )
+
+
+def test_visibility():
+    """Tests the GEO cases with continuous visibility and no visibility."""
+    # test init time
+    init_time = Time("2015-10-04T08:00:00.000", scale="utc")
+
+    duration = 1 * u.day
+
+    steps = 4000
+
+    # GEO with full visibility
+    events = event_engine(generate_geo_42e_trajectory, init_time, duration, steps)
+
+    print(f"full vis: {events.start_end_intervals}")
+
+    assert (
+        str(events.start_end_intervals.get_interval(0).start)
+        == "2015-10-04T08:00:00.000"
+    )
+
+    assert (
+        str(events.start_end_intervals.get_interval(0).end) == "2015-10-05T07:59:38.400"
+    )
+
+    # GEO with no visibility
+    events = event_engine(generate_geo_140w_trajectory, init_time, duration, steps)
+
+    print(f"no vis: {events.start_end_intervals}")
+
+    assert not events.start_end_intervals.intervals
+
+
+def test_init_array_mismatch():
+    """Tests the input error due to array size mismatch."""
+    with pytest.raises(ValueError):
+        time_list = prepare_timelist(
+            Time("2015-10-04T00:00:00.000", scale="utc"), 1.0 * u.day, 1000
+        )["time_list"]
+        events = DiscreteTimeIntervals(time_list, [0.1, 1.2, 2.3])
