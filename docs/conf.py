56db7a034ed51bbb14847cd88e1284568e5de133
egemenimre <s.egemen.imre@gmail.com>
2020-07-12 00:38:59 +0200

    bumped version to 0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ author = "Egemen Imre"
 # Version Info
 # ------------
 # The short X.Y version.
-version = "0.0.3"
+version = "0.0.4"
 # The full version, including alpha/beta/rc tags.
-release = "0.0.3"
+release = "0.0.4"
 
 # -- General configuration ---------------------------------------------------
 # By default, highlight as Python 3.
