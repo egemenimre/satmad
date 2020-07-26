4805178192b7f567bb86795e67daf8b1b6de74e4
egemenimre <s.egemen.imre@gmail.com>
2020-07-26 22:58:36 +0200

    Init commit with two-body.

---
+++ satmad/propagation/force_models.py
@@ -0,0 +1,89 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Force models generally for use with the numerical propagators.
+
+"""
+import numpy as np
+from astropy import units as u
+
+
+def two_body_accel(r, mu):
+    r"""
+    Two-body acceleration.
+
+    The acceleration is in in the inertial frame of the central body. For the
+    Earth this is GCRS.
+
+    The acceleration value is given by:
+
+    .. math::
+
+        \ddot{\vec{r}} = - \dfrac{\mu}{r^3} \vec{r}
+
+    where :math:`\ddot{\vec{r}}` is the inertial acceleration,
+    :math:`\vec{r}` is the position vector (with :math:`r` its norm).
+    :math:`\mu` is equal to the constant :math:`GM` (Gravitational Constant times
+    the Mass of the main attracting body.)
+
+    .. note::
+        This method is used in the Scipy ODE solver, therefore it does not accept
+        `Quantity` inputs for efficiency. Care must be taken with the units.
+
+
+    Parameters
+    ----------
+    r : ndarray
+        position vector with 3 elements (km)
+    mu : float
+        GM value (:math:`km^3 / s^2`)
+
+    Returns
+    -------
+    float
+        acceleration (:math:`km / s^2`)
+
+    """
+    r_norm = np.linalg.norm(r)
+    a = -mu / (r_norm ** 3) * r
+    return a
+
+
+def two_body_energy(r, v, mu=398600.5 * u.km ** 3 / u.s ** 2):
+    r"""Two-body specific energy.
+
+    The specific energy value is given by:
+
+    .. math::
+
+        \varepsilon = \dfrac{v^2}{2} - \dfrac{\mu}{r}
+
+    with :math:`v` is the norm of the velocity vector, :math:`r` is the norm of the
+    position vector and :math:`\mu` is equal to the constant :math:`GM`
+    (Gravitational Constant times the Mass of the main attracting body).
+
+    This is the formula for the specific energy, or the sum of
+    potential and kinetic energy *per unit mass*.
+
+    Parameters
+    ----------
+    r : ndarray or Quantity
+        position vector with 3 elements (km)
+    v : ndarray or Quantity
+        position vector with 3 elements (km/s)
+    mu : float or Quantity
+        GM value (:math:`km^3 / s^2`)
+
+    Returns
+    -------
+    sp_energy : float or Quantity
+        specific energy (:math:`km^2 / s^2`)
+    """
+    r_norm = np.linalg.norm(r)
+    v_norm = np.linalg.norm(v)
+    sp_energy = 0.5 * v_norm ** 2 - mu / r_norm
+
+    return sp_energy
