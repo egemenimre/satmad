fcd873bfed393b995c2f1cfbe8252ebd8d600f0e
egemenimre <s.egemen.imre@gmail.com>
2020-11-04 00:11:42 +0100

    Updated intersphinx links and increased nbsphinx timeout to 1000 sec.

--- docs/conf.py
+++ docs/conf.py
@@ -63,8 +63,8 @@ master_doc = "index"
 # Intersphinx configuration
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "astropy": ("http://docs.astropy.org/en/stable/", None),
-    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "astropy": ("https://docs.astropy.org/en/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
     "matplotlib": ("https://matplotlib.org", None),
 }
@@ -75,7 +75,7 @@ else:
     nbsphinx_execute = "always"
 
 # Controls when a cell will time out (defaults to 30; use -1 for no timeout):
-nbsphinx_timeout = 60
+nbsphinx_timeout = 1000
 
 # -- Options for HTML output -------------------------------------------------
 
