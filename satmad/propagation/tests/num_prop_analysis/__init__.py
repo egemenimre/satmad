98e2dc4c0c60aea08fe3734fe489f85e54167fe8
egemenimre <s.egemen.imre@gmail.com>
2021-03-25 22:40:28 +0100

    Moved energy conservation analyses to applications project, updated links

--- satmad/propagation/tests/num_prop_analysis/__init__.py
+++
@@ -1,9 +0,0 @@
-# SatMAD: Satellite Mission Analysis and Design for Python
-#
-# Copyright (C) 2020 Egemen Imre
-#
-# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
-"""
-Numerical propagation analyses.
-
-"""
