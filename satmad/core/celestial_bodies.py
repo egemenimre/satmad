6dd60716a129f3454aa9ab9928d43fe4709f2b67
egemenimre <s.egemen.imre@gmail.com>
2021-04-10 00:02:02 +0200

    Fixed WGS84 link and added some values.

--- satmad/core/celestial_bodies.py
+++ satmad/core/celestial_bodies.py
@@ -17,7 +17,8 @@ from satmad.core.central_body import CelestialBody, CelestialBodyEllipsoid
 
 GM_earth = 3.986004418e14 * u.m ** 3 / u.s ** 2
 """Geocentric gravitational constant
-(TCG-compatible value).
+(TCG-compatible value). Also compatible with the WGS84 coordinate system.
+Includes mass of the atmosphere.
 
 (See IERS Technical Note No. 36 (2010) Table 1.1 [TCF1]_)"""
 
@@ -61,11 +62,17 @@ EARTH_ELLIPSOID_GRS80 = CelestialBodyEllipsoid(
 (See IERS Technical Note No. 36 (2010) Table 1.2  [TCF1]_)."""
 
 EARTH_ELLIPSOID_WGS84 = CelestialBodyEllipsoid(
-    "Earth Ellipsoid WGS84", 6378137 * u.m, 298.257223563 * u.dimensionless_unscaled
+    "Earth Ellipsoid WGS84",
+    63781370 * u.m,
+    298.257223563 * u.dimensionless_unscaled,
+    mu=3.986004418e14 * u.m ** 3 / u.s ** 2,
+    j2=1.082629821313e-3 * u.dimensionless_unscaled,
+    om=7.292115e-5 * u.rad / u.s,
 )
 """Earth Ellipsoid defined in World Geodetic System WGS84.
 
-(See `WGS84 definition <https://earth-info.nga.mil/GandG/publications/tr8350.2/tr8350_2.html>`_)."""
+(See `WGS84 definition
+<https://earth-info.nga.mil/coordsys/coord-download.php?file=website/NGA.STND.0036_1.0.0_WGS84.pdf>`_)."""
 
 MOON_ELLIPSOID_IAUWG2015 = CelestialBodyEllipsoid(
     "Moon Ellipsoid IAU WG 2015", 1737400 * u.m, inf * u.dimensionless_unscaled
