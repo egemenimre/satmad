8991e57f3295f13bd42d7d1c4a485d1f47059ad0
egemenimre <s.egemen.imre@gmail.com>
2021-04-13 21:15:18 +0200

    Init classical orbital elems

---
+++ satmad/propagation/classical_orb_elems.py
@@ -0,0 +1,516 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2021 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Classical Orbital Elements definitions.
+
+"""
+from abc import ABC
+
+import numpy as np
+from astropy import units as u
+from astropy.coordinates import CartesianRepresentation
+from astropy.time import Time
+from astropy.units import Quantity
+
+from satmad.core.celestial_bodies import EARTH
+
+
+class AbstractKeplerianOrbitElements(ABC):
+    """
+    Classical (Keplerian) Orbital Elements that should be overridden with the specific
+    type (Mean, Osculating etc.).
+
+    Parameters
+    ----------
+    central_body : CelestialBody
+        celestial body (e.g. Earth) around which the orbit is defined
+    epoch : Time
+        Epoch Time corresponding to the orbital elements
+    sm_axis: Quantity
+        semimajor axis of the orbit [km]
+    inclination : Quantity
+        inclination of the orbit [rad]
+    raan : Quantity
+        right ascension of ascending node (RAAN) of the orbit [rad]
+    eccentricity : Quantity or float
+        eccentricity of the orbit [dimensionless]
+    arg_periapsis : Quantity
+        argument of periapsis [rad]
+    true_anomaly : Quantity
+        true anomaly of the orbit [rad]
+    """
+
+    _arg_periapsis: u.rad
+    _raan: u.rad
+    _true_anomaly: u.rad
+
+    def __init__(
+        self,
+        epoch,
+        sm_axis,
+        eccentricity,
+        inclination,
+        raan,
+        arg_periapsis,
+        true_anomaly,
+        central_body=EARTH,
+    ):
+
+        # Fill init values with setters
+        # Setters carry out the type and value checks
+        # Ignore codestyle checks as mypy thinks these are read only
+        self.central_body = central_body
+        self.epoch = epoch
+        self.sm_axis = sm_axis  # type: ignore
+        self.eccentricity = eccentricity
+        self.inclination = inclination  # type: ignore
+        self.raan = raan  # type: ignore
+        self.arg_periapsis = arg_periapsis  # type: ignore
+        self.true_anomaly = true_anomaly  # type: ignore
+
+    @property
+    def epoch(self) -> Time:
+        """Returns the epoch time associated with the orbital parameters.
+        Setter replicates the time with `copy=False`."""
+        return self._epoch
+
+    @epoch.setter
+    def epoch(self, epoch: Time):
+        self._epoch = epoch.replicate()
+
+    @property  # type: ignore
+    @u.quantity_input
+    def sm_axis(self) -> u.km:
+        """Gets the semimajor axis [km]."""
+        return self._sm_axis
+
+    @sm_axis.setter  # type: ignore
+    @u.quantity_input(sm_axis="length")
+    def sm_axis(self, sm_axis):
+        self._sm_axis = sm_axis
+
+    @property
+    def eccentricity(self) -> float:
+        """Gets and sets the eccentricity of the orbit.
+
+        Input can be a Quantity (dimensionless) or float. Getter value is a float.
+
+        Eccentricity should be in range 0 <= e < 1.0.
+        Raises a `ValueError` otherwise."""
+        return self._eccentricity
+
+    @eccentricity.setter
+    def eccentricity(self, e):
+
+        # check eccentricity as Quantity and reduce to value
+        if isinstance(e, Quantity):
+            e = e.value
+
+        # If ecc_mag too close to 1 then the orbit is parabolic.
+        if np.abs(1 - e) < 1e-7:
+            raise ValueError(
+                "Orbit eccentricity is close to 1. Parabolic orbits are not allowed."
+            )
+
+        if 0 <= e:
+            self._eccentricity = e
+        else:
+            raise ValueError(
+                f"Given argument ({e}) is an invalid "
+                f"Eccentricity value, "
+                f"only values in range 0 <= e < 1.0 are allowed."
+            )
+
+    @property  # type: ignore
+    @u.quantity_input
+    def inclination(self) -> u.rad:
+        """Gets and sets the inclination of the orbit [rad].
+
+        Inclination should be in range 0 <= om < PI.
+        Raises a `ValueError` otherwise.
+        """
+        return self._incl
+
+    @inclination.setter  # type: ignore
+    @u.quantity_input(incl="angle")
+    def inclination(self, incl):
+
+        if 0 <= incl.to_value(u.rad) < np.pi:
+            self._incl = incl
+        else:
+            raise ValueError(
+                f"Given argument ({incl}) is an invalid "
+                f"Inclination value, "
+                f"only values in range 0 <= i < PI are allowed."
+            )
+
+    @property  # type: ignore
+    @u.quantity_input
+    def raan(self) -> u.rad:
+        """Gets and sets the right ascension of ascending node (RAAN)
+        of the orbit [rad].
+
+        RAAN should be in range 0 <= om < 2*PI. Any input outside this range will
+        be forced into this range."""
+        return self._raan
+
+    @raan.setter  # type: ignore
+    @u.quantity_input(om="angle")
+    def raan(self, om):
+        self._raan = _force_angle_range(om)
+
+    @property  # type: ignore
+    @u.quantity_input
+    def arg_periapsis(self) -> u.rad:
+        """Gets and sets the argument of periapsis [rad].
+
+        Argument of periapsis should be in range 0 <= argp < 2*PI.
+        Any input outside this range will
+        be forced into this range."""
+        return self._arg_periapsis
+
+    @arg_periapsis.setter  # type: ignore
+    @u.quantity_input(argp="angle")
+    def arg_periapsis(self, argp):
+        self._arg_periapsis = _force_angle_range(argp)
+
+    @property  # type: ignore
+    @u.quantity_input
+    def true_anomaly(self) -> u.rad:
+        """Gets and sets the true anomaly of the orbit [rad].
+
+        True Anomaly should be in range 0 <= true_anomaly < 2*PI.
+        Any input outside this range will
+        be forced into this range."""
+        return self._true_anomaly
+
+    @true_anomaly.setter  # type: ignore
+    @u.quantity_input(true_anomaly="angle")
+    def true_anomaly(self, true_anomaly):
+        self._true_anomaly = _force_angle_range(true_anomaly)
+
+    @property
+    def mean_motion(self):
+        """Computes the mean motion of the orbit [rad/sec].
+
+        Returns
+        -------
+        Quantity
+            mean motion in u.rad / u.s
+        """
+        return np.sqrt(self.central_body.mu / np.abs(self.sm_axis ** 3)).to(
+            u.rad / u.s, equivalencies=u.dimensionless_angles()
+        )
+
+    @property  # type: ignore
+    @u.quantity_input
+    def period(self) -> u.s:
+        """Computes the satellite period [sec]."""
+        return 2 * np.pi * u.rad / self.mean_motion
+
+    @property  # type: ignore
+    @u.quantity_input
+    def periapsis(self) -> u.km:
+        """Computes the periapsis distance [km]."""
+        return self.sm_axis * (1 - self.eccentricity)
+
+    @property  # type: ignore
+    @u.quantity_input
+    def apoapsis(self) -> u.km:
+        """Computes the apoapsis distance [km]."""
+        return self.sm_axis * (1 + self.eccentricity)
+
+
+class OsculatingKeplerianOrbElems(AbstractKeplerianOrbitElements):
+    """
+    Osculating Classical (Keplerian) Orbital Elements.
+
+    By definition, this uses a two-body potential for computations. Therefore, this is
+    not a "mean elements" model such as a TLE. Over a trajectory generated with a
+    two-body force model, the orbital elements (apart from true anomaly) should stay
+    constant, limited by the accuracy of the trajectory generation algorithm.
+
+    Osculating orbital elements should be used with care,
+    particularly in contexts where instantaneous orbital elements are computed on a
+    trajectory generated by a non-two-body model (e.g. including geopotentials). The
+    orbital elements will *not* stay constant along the trajectory, simply because the
+    force model over successive points are not strictly two-body.
+
+    Parameters
+    ----------
+    central_body : CelestialBody
+        celestial body (e.g. Earth) around which the orbit is defined
+    epoch : Time
+        Epoch Time corresponding to the orbital elements
+    sm_axis: Quantity
+        semimajor axis of the orbit [km]
+    inclination : Quantity
+        inclination of the orbit [rad]
+    raan : Quantity
+        right ascension of ascending node (RAAN) of the orbit [rad]
+    eccentricity : Quantity or float
+        eccentricity of the orbit [dimensionless]
+    arg_periapsis : Quantity
+        argument of periapsis [rad]
+    true_anomaly : Quantity
+        true anomaly of the orbit [rad]
+    """
+
+    @u.quantity_input(
+        sm_axis="length",
+        inclination="angle",
+        raan="angle",
+        arg_periapsis="angle",
+        true_anomaly="angle",
+    )
+    def __init__(
+        self,
+        epoch,
+        sm_axis,
+        eccentricity,
+        inclination,
+        raan,
+        arg_periapsis,
+        true_anomaly,
+        central_body=EARTH,
+    ):
+        super().__init__(
+            epoch,
+            sm_axis,
+            eccentricity,
+            inclination,
+            raan,
+            arg_periapsis,
+            true_anomaly,
+            central_body=central_body,
+        )
+
+    @classmethod
+    def from_cartesian(cls, init_coords, central_body=EARTH):
+        """
+        Generates Osculating Keplerian Elements from cartesian coordinates.
+
+        If the initial coordinate is in a different frame than the propagation frame,
+        it is automatically converted to the proper frame for propagation.
+
+        Parameters
+        ----------
+        init_coords : SkyCoord
+            Initial coordinates (the first value is used)
+        central_body : CelestialBody
+            celestial body (e.g. Earth) around which the orbit is defined
+
+        Returns
+        -------
+        OsculatingKeplerianOrbElems
+            Osculating classical (or Keplerian) orbital elements
+
+        Raises
+        ------
+        ValueError
+            Parabolic orbits or singularity
+        """
+
+        # Check initial conditions
+        if init_coords.isscalar:
+            # Single SkyCoord element available
+            init_coords_converted = init_coords
+        else:
+            # Multiple SkyCoord elements available, use first
+            init_coords_converted = init_coords[0]
+
+        # Check for the initial coordinate system, convert if necessary
+        if central_body.inert_coord != init_coords_converted.frame.name:
+            init_coords_converted = init_coords_converted.transform_to(
+                central_body.inert_coord
+            )
+
+        # Compute orbital elements
+        orb_elems = _rv_to_keplerian_elems(
+            init_coords_converted.obstime,
+            init_coords_converted.cartesian.without_differentials(),
+            init_coords_converted.velocity,
+            central_body,
+        )
+
+        return orb_elems
+
+
+def _rv_to_keplerian_elems(epoch, r, v, central_body):
+    """
+    Converts position and velocity vectors to an Osculating Keplerian Orbital Elems
+    object.
+
+    This section is adapted from GMAT Math Specifications Draft for R2020a Section 3.1.2
+    (6 Mar 2020) http://gmat.sourceforge.net/doc/R2020a/GMATMathSpec.pdf.
+
+    Parameters
+    ----------
+    epoch : Time
+        Epoch Time corresponding to the orbital elements
+    r: CartesianRepresentation
+        Position vector
+    v: CartesianDifferential
+        Velocity vector
+    central_body : CelestialBody
+        celestial body (e.g. Earth) around which the orbit is defined
+
+    Returns
+    -------
+    OsculatingKeplerianOrbElems
+        Osculating classical (or Keplerian) orbital elements
+
+    Raises
+    ------
+    ValueError
+        Parabolic orbits or singularity
+    """
+
+    # shorthands
+    mu = central_body.mu
+    r_mag = r.norm()
+    v_mag = v.norm()
+
+    # ang mom
+    h = r.cross(v)
+    h_mag = h.norm()
+
+    # k-axis unit vector (Z-axis of the coord frame)
+    k = CartesianRepresentation([0, 0, 1])
+
+    # orbit normal
+    n = k.cross(h)
+    n_mag = n.norm()
+
+    # find eccentricity vector and magnitude
+    ecc = ((v_mag ** 2 - mu / r_mag) * r - r.dot(v) * v) / mu
+    ecc_mag = ecc.norm().decompose()
+
+    # Find two-body energy
+    energy = 0.5 * v_mag ** 2 - mu / r_mag
+
+    # If ecc_mag too close to 1  then the orbit is parabolic. Abort the conversion.
+    if np.abs(1 - ecc_mag) < 1e-7:
+        raise ValueError(
+            "Orbit eccentricity is equal to 1. Parabolic orbits are not allowed."
+        )
+
+    # compute sm_axis
+    sm_axis = -mu / (2 * energy)
+
+    # If conic section is too close to singular, then conversion is to be aborted.
+    if np.abs(sm_axis * (1 - ecc_mag)) < 1e-3 * u.km:
+        raise ValueError(
+            "Orbit periapsis too close to zero. Singular conic sections are not allowed."
+        )
+
+    # compute the inclination
+    i = np.arccos(h.z / h_mag)
+
+    # The rest depends on ellipticity and inclination
+    is_circular = True if ecc_mag < 1e-11 else False
+    is_equatorial = False if (1e-11 * u.rad <= i <= (np.pi - 1e-11) * u.rad) else True
+
+    # *** Case 1: Non-circular, Inclined Orbit ***
+    if not is_circular and not is_equatorial:
+
+        # Compute RAAN and fix quadrant
+        raan = np.arccos(n.x / n_mag)
+        if n.y < 0:
+            raan = 2 * np.pi * u.rad - raan
+
+        # Compute argument of periapsis and fix quadrant
+        arg_p = np.arccos(n.dot(ecc) / (n_mag * ecc_mag))
+        if ecc.z < 0:
+            arg_p = 2 * np.pi * u.rad - arg_p
+
+        # Compute true anomaly and fix quadrant
+        true_an = np.arccos(ecc.dot(r) / (ecc_mag * r_mag))
+        if r.dot(v) < 0:
+            true_an = 2 * np.pi * u.rad - true_an
+
+    # *** Case 2: Non-circular, Equatorial Orbit ***
+    elif not is_circular and is_equatorial:
+        # Set RAAN to zero
+        raan = 0 * u.rad
+
+        # Compute argument of periapsis and fix quadrant
+        arg_p = np.arccos(ecc.x / ecc_mag)
+        if ecc.y < 0:
+            arg_p = 2 * np.pi * u.rad - arg_p
+
+        # Compute true anomaly and fix quadrant
+        true_an = np.arccos(ecc.dot(r) / (ecc_mag * r_mag))
+        if r.dot(v) < 0:
+            true_an = 2 * np.pi * u.rad - true_an
+
+    # *** Case 3: Circular, Inclined Orbit ***
+    elif is_circular and not is_equatorial:
+
+        # Compute RAAN and fix quadrant
+        raan = np.arccos(n.x / n_mag)
+        if n.y < 0:
+            raan = 2 * np.pi * u.rad - raan
+
+        # Set argument of periapsis to zero
+        arg_p = 0 * u.rad
+
+        # Compute true anomaly and fix quadrant
+        true_an = np.arccos(n.dot(r) / (n_mag * r_mag))
+        if r.z < 0:
+            true_an = 2 * np.pi * u.rad - true_an
+
+    # *** Case 4: Circular, Equatorial Orbit ***
+    else:
+        # Set RAAN to zero
+        raan = 0 * u.rad
+
+        # Set argument of periapsis to zero
+        arg_p = 0 * u.rad
+
+        # Compute true anomaly and fix quadrant
+        true_an = np.arccos(r.x / r_mag)
+        if r.y < 0:
+            true_an = 2 * np.pi * u.rad - true_an
+
+    return OsculatingKeplerianOrbElems(
+        epoch,
+        sm_axis,
+        ecc_mag,
+        i,
+        raan,
+        arg_p,
+        true_an,
+        central_body,
+    )
+
+
+@u.quantity_input(angle_rad="angle", min_range="angle", max_range="angle")
+def _force_angle_range(
+    angle, min_range=0 * u.rad, max_range=2 * np.pi * u.rad
+) -> u.rad:
+    """
+    Forces the angles into a prescribed range.
+
+    Parameters
+    ----------
+    angle : Quantity
+        Angle [rad]
+    min_range : Quantity
+        Minimum value allowable [rad]
+    max_range : Quantity
+        Max value allowable  [rad]
+
+    """
+
+    while not (min_range <= angle < max_range):
+        if max_range < angle:
+            angle = angle - 2 * np.pi * u.rad
+
+        if angle < min_range:
+            angle = angle + 2 * np.pi * u.rad
+
+    return angle
---
+++ satmad/propagation/tests/test_classical_orb_elems.py
@@ -0,0 +1,346 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2021 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Test orbit definitions.
+
+"""
+
+import pytest
+from astropy import units as u
+from astropy.coordinates import (
+    GCRS,
+    ITRS,
+    CartesianDifferential,
+    CartesianRepresentation,
+)
+from astropy.time import Time
+from pytest import approx
+
+from satmad.coordinates.frames import init_rvt
+from satmad.core.celestial_bodies import EARTH
+from satmad.core.central_body import CelestialBody
+from satmad.propagation.classical_orb_elems import (
+    AbstractKeplerianOrbitElements,
+    OsculatingKeplerianOrbElems,
+)
+
+_EARTH_GMAT = CelestialBody(
+    "Earth",
+    "Earth as depicted in GMAT.",
+    3.98600441500000e14 * u.m ** 3 / u.s ** 2,
+    inert_coord="gcrs",
+)
+
+
+@pytest.fixture
+def init_elems_leo():
+    """Init a LEO example."""
+
+    time = Time("2020-01-11T11:00:00.000", scale="utc")
+    central_body = EARTH
+
+    sm_axis = 7055.95320378 * u.km
+    ecc = 0.0020835 * u.dimensionless_unscaled
+    incl = 1.71234602 * u.rad
+    raan = 4.42402394 * u.rad
+    arg_perigee = 5.23982923 * u.rad
+    true_an = 1.5 * u.rad
+
+    orb_elems = AbstractKeplerianOrbitElements(
+        time, sm_axis, ecc, incl, raan, arg_perigee, true_an, central_body
+    )
+
+    return orb_elems
+
+
+def _parse_rv_line(rv_line, coord_sys):
+    """Converts a line of t, r, v string into a SkyCoord object."""
+
+    rv_items = [elem for elem in rv_line.strip().split(" ") if elem]
+
+    time = Time(rv_items[0], scale="utc")
+
+    v = CartesianDifferential(
+        [float(rv_items[4]), float(rv_items[5]), float(rv_items[6])],
+        unit=u.km / u.s,
+    )
+    r = CartesianRepresentation(
+        [float(rv_items[1]), float(rv_items[2]), float(rv_items[3])], unit=u.km
+    )
+
+    rv_init = init_rvt(coord_sys, time, r.with_differentials(v))
+
+    return rv_init
+
+
+def _parse_elems_line(epoch, elems_line, central_body):
+    """Converts a line of elems string into a OsculatingOrbitalElems object."""
+
+    elem_items = [elem for elem in elems_line.strip().split(" ") if elem]
+
+    orb_elems = OsculatingKeplerianOrbElems(
+        epoch,
+        sm_axis=float(elem_items[1]) * u.km,
+        eccentricity=float(elem_items[2]),
+        inclination=float(elem_items[3]) * u.deg,
+        raan=float(elem_items[4]) * u.deg,
+        arg_periapsis=float(elem_items[5]) * u.deg,
+        true_anomaly=float(elem_items[6]) * u.deg,
+        central_body=central_body,
+    )
+
+    return orb_elems
+
+
+def _elems_assert(osc_elems, truth_elems, errs):
+    """Tests the orbital elements with given tolerances"""
+    assert osc_elems.sm_axis.to_value(u.km) == approx(
+        truth_elems.sm_axis.to_value(u.km), abs=errs[0].to_value(u.km)
+    )
+
+    assert osc_elems.eccentricity == approx(truth_elems.eccentricity, abs=errs[1])
+
+    assert osc_elems.inclination.to_value(u.deg) == approx(
+        truth_elems.inclination.to_value(u.deg), abs=errs[2].to_value(u.deg)
+    )
+
+    assert osc_elems.raan.to_value(u.deg) == approx(
+        truth_elems.raan.to_value(u.deg), abs=errs[3].to_value(u.deg)
+    )
+
+    assert osc_elems.arg_periapsis.to_value(u.deg) == approx(
+        truth_elems.arg_periapsis.to_value(u.deg), abs=errs[4].to_value(u.deg)
+    )
+
+    assert osc_elems.true_anomaly.to_value(u.deg) == approx(
+        truth_elems.true_anomaly.to_value(u.deg), abs=errs[5].to_value(u.deg)
+    )
+
+
+def __init_ell_incl():
+    """Case 1: Initialises an Earth Elliptical Inclined orbit satellite. GMAT example.
+
+    This case deliberately uses an ITRS input, degrading the agreement between GMAT and SatMAD."""
+
+    rv_line = "2010-01-01T13:24:28.100   3769.438525258299  6010.414642548296   1306.35068662678  -5.810744579719358   3.604099244744345   0.9947338414920035"
+    gmat_elems_line = "25198.05865856482         7191.999999999875         0.02000000000000014       12.84999999999999         306.6                      314.1899999999973          99.8870000000026         "
+
+    rv_init = _parse_rv_line(rv_line, ITRS)
+    gmat_elems = _parse_elems_line(rv_init.obstime, gmat_elems_line, _EARTH_GMAT)
+
+    return (
+        rv_init,
+        gmat_elems,
+        [
+            9 * u.mm,  # sm axis
+            1e-9,  # ecc
+            5e-7 * u.deg,  # inc
+            5e-6 * u.deg,  # raan
+            5e-6 * u.deg,  # argp
+            3e-6 * u.deg,  # true an
+        ],
+    )
+
+
+def __init_ell_equator():
+    """Case 2: Initialises an Earth Elliptical Equatorial orbit satellite. GMAT example."""
+
+    rv_line = "2010-01-01T13:24:28.100   7.213392947764267e+03   8.523654531348812e+01  -2.783146976770290e-16   5.902225938368851e-02   7.421779936019859e+00   1.595360086373873e-18"
+    gmat_elems_line = "25198.05865856482         7191.999999999999         0.01999999999999993       0.0         0.0                      260.7899999999987          99.88700000000058         "
+
+    rv_init = _parse_rv_line(rv_line, GCRS)
+    gmat_elems = _parse_elems_line(rv_init.obstime, gmat_elems_line, _EARTH_GMAT)
+
+    return (
+        rv_init,
+        gmat_elems,
+        [
+            0.0001 * u.mm,  # sm axis
+            1e-14,  # ecc
+            1e-14 * u.deg,  # inc
+            1e-14 * u.deg,  # raan
+            5e-13 * u.deg,  # argp
+            1e-12 * u.deg,  # true an
+        ],
+    )
+
+
+def __init_circ_incl():
+    """Case 3: Initialises an Earth Circular Inclined orbit satellite. GMAT example."""
+
+    rv_line = "2010-01-01T13:24:28.100   7.074397805771428e+03  -1.988417626249246e+00   1.295282105138377e+03  -1.757812701818108e-01   7.378906649083491e+00   9.713860595480043e-01"
+    gmat_elems_line = "25198.05865856482         7191.999999999996         2.02792751048046e-16      12.84999999999999         306.6                      0                         54.07699999999998         "
+
+    rv_init = _parse_rv_line(rv_line, GCRS)
+    gmat_elems = _parse_elems_line(rv_init.obstime, gmat_elems_line, _EARTH_GMAT)
+
+    return (
+        rv_init,
+        gmat_elems,
+        [
+            0.0001 * u.mm,  # sm axis
+            1e-14,  # ecc
+            1e-14 * u.deg,  # inc
+            1e-14 * u.deg,  # raan
+            5e-13 * u.deg,  # argp
+            1e-13 * u.deg,  # true an
+        ],
+    )
+
+
+def __init_circ_equator():
+    """Case 4: Initialises an Earth Circular Equatorial orbit satellite. GMAT example."""
+
+    rv_line = "2010-01-01T13:24:28.100   4.355971648857403e+03  -5.722794334444523e+03  -1.411143337598508e-15   5.923828927156149e+00   4.508991473634389e+00   7.270719060994101e-19"
+    gmat_elems_line = "25198.05865856482         7191.999999999996         2.02792751048046e-16      0.0         0.0                      0                         307.2770000000005     "
+
+    rv_init = _parse_rv_line(rv_line, GCRS)
+    gmat_elems = _parse_elems_line(rv_init.obstime, gmat_elems_line, _EARTH_GMAT)
+
+    return (
+        rv_init,
+        gmat_elems,
+        [
+            0.0001 * u.mm,  # sm axis
+            1e-14,  # ecc
+            1e-14 * u.deg,  # inc
+            1e-14 * u.deg,  # raan
+            1e-13 * u.deg,  # argp
+            1e-13 * u.deg,  # true an
+        ],
+    )
+
+
+def __init_hyperbolic_incl():
+    """Initialises an Earth Hyperbolic Inclined orbit satellite. GMAT example."""
+
+    rv_line = "2010-01-01T13:24:28.100   3.464897862367937e+02  -9.738869896647320e-02   6.344031422145902e+01   3.588822034885639e+01   3.027151368726605e+01   1.068941616196268e+01"
+    gmat_elems_line = "25198.05865856482         -7191.999999999876         1.02       12.84999999999999         306.6                      314.1899999999976          99.88700000000226         "
+
+    rv_init = _parse_rv_line(rv_line, GCRS)
+    gmat_elems = _parse_elems_line(rv_init.obstime, gmat_elems_line, _EARTH_GMAT)
+
+    return (
+        rv_init,
+        gmat_elems,
+        [
+            0.0001 * u.mm,  # sm axis
+            1e-14,  # ecc
+            1e-14 * u.deg,  # inc
+            5e-13 * u.deg,  # raan
+            1e-13 * u.deg,  # argp
+            1e-13 * u.deg,  # true an
+        ],
+    )
+
+
+@pytest.mark.parametrize(
+    "init_case",
+    [
+        "__init_ell_incl",
+        "__init_ell_equator",
+        "__init_circ_incl",
+        "__init_circ_equator",
+        "__init_hyperbolic_incl",
+    ],
+)
+def test_rv_to_osc_elems(init_case):
+    """Tests the various orbit cases."""
+
+    # find the function and run it
+    rv, gmat_elems, errs = globals()[init_case]()
+
+    osc_elems = OsculatingKeplerianOrbElems.from_cartesian(rv, _EARTH_GMAT)
+
+    _elems_assert(osc_elems, gmat_elems, errs)
+
+
+def test_parabolic(init_elems_leo):
+    """Tests eccentricity setter with input value = 1
+    - should raise `ValueError`."""
+    with pytest.raises(ValueError):
+        orb_elems = init_elems_leo
+        orb_elems.eccentricity = 1.0000000001
+
+
+def test_ecc_out_of_bounds(init_elems_leo):
+    """Tests eccentricity setter with input value out of bounds
+    - should raise `ValueError`."""
+    with pytest.raises(ValueError):
+        orb_elems = init_elems_leo
+        orb_elems.eccentricity = -1.2
+
+
+def test_incl_out_of_bounds(init_elems_leo):
+    """Tests inclination setter with input value out of bounds
+    - should raise `ValueError`."""
+    with pytest.raises(ValueError):
+        orb_elems = init_elems_leo
+        orb_elems.inclination = 210 * u.deg
+
+
+def test_raan_out_of_bounds(init_elems_leo):
+    """Tests RAAN setter with input value out of bounds """
+    orb_elems = init_elems_leo
+    orb_elems.raan = 390 * u.deg
+    assert orb_elems.raan.to_value(u.deg) == approx((30 * u.deg).to_value(), abs=1e-8)
+
+
+def test_getters_setters(init_elems_leo):
+    """Test getters and setters of the Orbit."""
+    orb_elems = init_elems_leo
+
+    # test getters
+    assert orb_elems.sm_axis.to_value() == approx(
+        (7055.95320378 * u.km).to_value(), abs=(0.01 * u.mm).to_value(u.km)
+    )
+
+    assert orb_elems.period.to_value(u.s) == approx(
+        (5898.539855583115 * u.s).to_value(), abs=(1 * u.us).to_value(u.s)
+    )
+    assert orb_elems.inclination.to_value(u.deg) == approx(
+        (1.71234602 * u.rad).to_value(u.deg), abs=1e-6
+    )
+    assert orb_elems.raan.to_value(u.deg) == approx(
+        (4.42402394 * u.rad).to_value(u.deg), abs=1e-8
+    )
+    assert orb_elems.eccentricity == approx(0.0020835, abs=1e-9)
+    assert orb_elems.true_anomaly.to_value(u.deg) == approx(
+        (1.5 * u.rad).to_value(u.deg), abs=1e-8
+    )
+    assert orb_elems.arg_periapsis.to_value(u.deg) == approx(
+        (5.23982923 * u.rad).to_value(u.deg), abs=1e-8
+    )
+
+    # test setters
+    orb_elems.inclination = 20.00 * u.deg
+    assert orb_elems.inclination.to_value(u.deg) == approx(
+        (20.00 * u.deg).to_value(), abs=1e-8
+    )
+
+    orb_elems.raan = 210.00 * u.deg
+    assert orb_elems.raan.to_value(u.deg) == approx(
+        (210.00 * u.deg).to_value(), abs=1e-8
+    )
+
+    orb_elems.arg_periapsis = 30.00 * u.deg
+    assert orb_elems.arg_periapsis.to_value(u.deg) == approx(
+        (30.00 * u.deg).to_value(), abs=1e-8
+    )
+
+    orb_elems.true_anomaly = 210.00 * u.deg
+    assert orb_elems.true_anomaly.to_value(u.deg) == approx(
+        (210.00 * u.deg).to_value(), abs=1e-8
+    )
+
+    orb_elems.eccentricity = 0.051
+    assert orb_elems.eccentricity == approx(0.051, abs=1e-10)
+
+    orb_elems.eccentricity = 0.051 * u.dimensionless_unscaled
+    assert orb_elems.eccentricity == approx(0.051, abs=1e-10)
+
+    orb_elems.sm_axis = 7000000 * u.m
+    assert orb_elems.sm_axis == 7000 * u.km
